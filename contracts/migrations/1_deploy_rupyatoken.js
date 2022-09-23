7ae443c21df5e588cb407593476cdb04091b9c2d
berginprem <76146666+berginprem@users.noreply.github.com>
2022-09-23 12:20:02 +0530

    Delete contracts directory

--- contracts/migrations/1_deploy_rupyatoken.js
+++
@@ -1,7 +0,0 @@
-const RupyaToken = artifacts.require("RupyaToken");
-
-module.exports = async function (deployer, network, accounts) {
-  // Deploy Rupya Token
-  await deployer.deploy(RupyaToken);
-  const rupyaToken = await RupyaToken.deployed();
-};
--- contracts/migrations/2_deploy_usdctoken.js
+++
@@ -1,7 +0,0 @@
-const USDCToken = artifacts.require("USDCToken");
-
-module.exports = async function (deployer, network, accounts) {
-  // Deploy USDC Token
-  await deployer.deploy(USDCToken);
-  const usdcToken = await USDCToken.deployed();
-};
